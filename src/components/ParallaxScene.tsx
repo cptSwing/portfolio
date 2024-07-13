997e6994e900ccc183741468488a92229a25dea6
Jens Brandenburg <jens@vragments.com>
2024-07-13 17:25:46 +0200

    edits

--- src/components/ParallaxScene.tsx
+++ src/components/ParallaxScene.tsx
@@ -7,7 +7,7 @@ const ParallaxScene = () => {
     // const [menuHomeIsClicked, setMenuHomeIsClicked] = menuHomeIsClickedState;
 
     useEffect(() => {
-        parallaxEffect(2);
+        parallaxEffect(10);
     }, []);
 
     return (
@@ -101,10 +101,11 @@ const ParallaxMenuLayer: FC<{
     return (
         <div
             className={classNames(
-                "pointer-events-none absolute bottom-0 left-0 right-0 top-0 m-auto transform overflow-hidden rounded-md transition-transform delay-500 duration-700 will-change-transform",
+                "pointer-events-none absolute bottom-0 left-0 right-0 top-0 m-auto transform overflow-hidden rounded-md transition-transform will-change-transform",
                 parallaxLevelClassName,
                 extraClassNames,
             )}
+            onMouseOver={(e) => console.log("%c[ParallaxScene]", "color: #29c793", `e.target :`, e.target)}
         >
             {content}
         </div>
@@ -126,13 +127,13 @@ const ParallaxQuadLayer = () => {
                         <div className="h-full w-1/5" />
                     </div>
 
-                    <div className="will-change-filter filter-bloom absolute bottom-0 left-1/2 mb-8 flex -translate-x-1/2 flex-col items-start justify-end rounded-md border-2 border-green-800 bg-green-1000 px-8 py-2">
+                    <div className="filter-bloom absolute bottom-0 left-1/2 mb-8 flex -translate-x-1/2 flex-col items-start justify-end rounded-md border-2 border-green-800 bg-green-1000 px-8 py-2 will-change-filter">
                         <div className="">jens brandenburg</div>
 
                         <div className="text-sm italic text-white/40">I build websites, I build 3D scenes. Then I combine the two.</div>
                     </div>
                 </div>
-                <div className="will-change-filter filter-bloom h-full w-[10%] border-b border-l border-green-900/50"></div>
+                <div className="filter-bloom h-full w-[10%] border-b border-l border-green-900/50 will-change-filter"></div>
             </div>
 
             {/* Middle row (horizontal) */}
@@ -181,36 +182,22 @@ const ParallaxMenu: FC<{ isEffectLayer: boolean; menuHomeIsClickedState: [boolea
     return (
         <>
             {/* Menu: */}
-            <ParallaxMenuLayer
-                key={4}
-                parallaxLevelClassName={"translate-z-36"}
-                extraClassNames=" !overflow-visible size-full"
-                content={
-                    isEffectLayer ? (
-                        <div className="ml-48 mr-auto mt-48">
-                            <TOCMenu menuHomeIsClickedState={menuHomeIsClickedState} />
-                        </div>
-                    ) : (
-                        <></>
-                    )
-                }
-            />
+            {isEffectLayer && <TopLeftMenu menuHomeIsClickedState={menuHomeIsClickedState} />}
 
             {/* Menu: */}
-            <ParallaxMenuLayer
-                key={3}
-                parallaxLevelClassName={"translate-z-28"}
-                extraClassNames=" !overflow-visible size-full"
-                content={
-                    isEffectLayer ? (
-                        <div className="mb-48 ml-auto mr-48">
+            {/* TODO This is stubbornly not working */}
+            {isEffectLayer && (
+                <ParallaxMenuLayer
+                    key={3}
+                    parallaxLevelClassName="translate-z-28"
+                    extraClassNames="!overflow-visible"
+                    content={
+                        <div className="absolute bottom-0 left-0 right-0 top-0 mb-24 size-full">
                             <TOCMenu menuHomeIsClickedState={menuHomeIsClickedState} />
                         </div>
-                    ) : (
-                        <></>
-                    )
-                }
-            />
+                    }
+                />
+            )}
 
             {/* Content: */}
             {isEffectLayer && (
@@ -218,7 +205,7 @@ const ParallaxMenu: FC<{ isEffectLayer: boolean; menuHomeIsClickedState: [boolea
                     key={0}
                     parallaxLevelClassName={"translate-z-0"}
                     extraClassNames={classNames(
-                        "p-6 transition-transform delay-200 duration-300  h-2/3 w-4/5",
+                        "p-6 delay-200 duration-300  h-2/3 w-4/5",
                         menuHomeIsClicked ? "-translate-x-full" : "translate-x-0",
                     )}
                     content={<Main />}
@@ -227,16 +214,15 @@ const ParallaxMenu: FC<{ isEffectLayer: boolean; menuHomeIsClickedState: [boolea
 
             <ParallaxMenuLayer
                 key={-2.5}
-                parallaxLevelClassName={classNames(menuHomeIsClicked ? "translate-z-16" : "-translate-z-16")}
-                extraClassNames="p-8  h-2/3 w-4/5"
+                parallaxLevelClassName={classNames(menuHomeIsClicked ? "translate-z-16 hover:translate-z-[4.5rem]" : "-translate-z-16")}
+                extraClassNames="p-8  h-2/3 w-4/5 delay-100 duration-500 hover:delay-0 hover:duration-100"
                 content={
                     <div
                         className={classNames(
-                            "will-change-filter filter-bloom h-1/5 w-1/5 translate-x-[400%] translate-y-1/4 rounded-md border-2 border-purple-400 p-1",
+                            "h-1/5 w-1/5 translate-x-[400%] translate-y-1/4 rounded-md border-2 border-purple-400 p-1",
                             isEffectLayer
-                                ? "pointer-events-auto cursor-pointer hover:border-2 hover:border-green-600"
-                                : "pointer-events-none bg-gray-400",
-                            menuHomeIsClicked ? "z-50" : "z-0",
+                                ? "filter-bloom pointer-events-auto cursor-pointer will-change-filter hover:border-2 hover:border-green-600"
+                                : "bg-gray-400",
                         )}
                     >
                         {!isEffectLayer && "Menu Item #2"}
@@ -246,40 +232,75 @@ const ParallaxMenu: FC<{ isEffectLayer: boolean; menuHomeIsClickedState: [boolea
 
             <ParallaxMenuLayer
                 key={-3.5}
-                parallaxLevelClassName={classNames(menuHomeIsClicked ? "translate-z-16" : "-translate-z-40")}
-                extraClassNames="p-8  h-2/3 w-4/5"
+                parallaxLevelClassName={classNames(menuHomeIsClicked ? "translate-z-16 hover:translate-z-[4.5rem]" : "-translate-z-40")}
+                extraClassNames="p-8  h-2/3 w-4/5 delay-100 duration-500 hover:delay-0 hover:duration-100"
                 content={
                     <div
                         className={classNames(
-                            "will-change-filter filter-bloom h-1/5 w-1/5 translate-x-[300%] translate-y-[400%] rounded-md border-2 border-red-900 p-1",
+                            "h-1/5 w-1/5 translate-x-[300%] translate-y-[400%] rounded-md border-2 border-red-900 p-1",
                             isEffectLayer
-                                ? "pointer-events-auto cursor-pointer hover:border-2 hover:border-red-400"
-                                : "pointer-events-none bg-gray-400",
-                            menuHomeIsClicked ? "z-50" : "z-0",
+                                ? "filter-bloom pointer-events-auto cursor-pointer will-change-filter hover:border-2 hover:border-red-400"
+                                : "bg-gray-400 hover:bg-yellow-500",
                         )}
                     >
-                        {/* {!isEffectLayer && "Menu Item #3"} */}
-                        Menu Item #3
+                        {!isEffectLayer && "Menu Item #3"}
                     </div>
                 }
             />
 
             <ParallaxMenuLayer
                 key={-1.5}
-                parallaxLevelClassName={classNames(menuHomeIsClicked ? "translate-z-16" : "-translate-z-8")}
-                extraClassNames="p-8  h-2/3 w-4/5"
+                parallaxLevelClassName={classNames(menuHomeIsClicked ? "translate-z-16 hover:translate-z-[4.5rem]" : "-translate-z-8")}
+                extraClassNames="p-8  h-2/3 w-4/5 delay-100 duration-500 hover:delay-0 hover:duration-100"
                 content={
                     <div
                         className={classNames(
                             "h-1/5 w-1/5 translate-x-[0%] translate-y-[100%] rounded-md border-2 border-dashed border-transparent p-1 outline outline-offset-2 outline-green-800/50",
-                            "",
                             isEffectLayer
                                 ? "pointer-events-auto cursor-pointer hover:border-2 hover:border-l-white hover:border-t-white hover:outline-4 hover:outline-green-800/70"
                                 : "bg-gray-400 outline-2",
-                            menuHomeIsClicked ? "z-50" : "z-0",
                         )}
                     >
                         {isEffectLayer && <h2>Menu Item #1</h2>}
+                    </div>
+                }
+            />
+        </>
+    );
+};
+
+const TopLeftMenu: FC<{
+    menuHomeIsClickedState: [boolean, React.Dispatch<React.SetStateAction<boolean>>];
+}> = ({ menuHomeIsClickedState }) => {
+    return (
+        <>
+            <ParallaxMenuLayer
+                key={4}
+                parallaxLevelClassName={"translate-z-36"}
+                extraClassNames="!overflow-visible size-full"
+                content={
+                    <div className="ml-48 mr-auto mt-32 h-full">
+                        <TOCMenu menuHomeIsClickedState={menuHomeIsClickedState} />
+                    </div>
+                }
+            />
+            <ParallaxMenuLayer
+                key={3.9}
+                parallaxLevelClassName={"translate-z-32"}
+                extraClassNames="!overflow-visible size-full"
+                content={
+                    <div className="ml-48 mr-auto mt-32 h-full">
+                        <div className="size-full border-2 border-gray-600"></div>
+                    </div>
+                }
+            />
+            <ParallaxMenuLayer
+                key={3.8}
+                parallaxLevelClassName={"translate-z-28"}
+                extraClassNames="!overflow-visible size-full"
+                content={
+                    <div className="ml-48 mr-auto mt-32 h-full">
+                        <div className="size-full border-2 border-gray-600"></div>
                     </div>
                 }
             />
@@ -291,7 +312,7 @@ const TOCMenu: FC<{ menuHomeIsClickedState: [boolean, React.Dispatch<React.SetSt
     const [, setMenuHomeIsClicked] = menuHomeIsClickedState;
 
     return (
-        <div className="pointer-events-auto w-1/5 border-2 border-slate-400 p-1.5">
+        <div className="filter-bloom pointer-events-auto w-1/5 border-2 border-slate-400 p-1.5 will-change-filter">
             <label className="relative size-full">
                 <input
                     type="checkbox"
