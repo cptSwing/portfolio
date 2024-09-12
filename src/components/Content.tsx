5ebde758e54139b0dffcdf87fc4b9d74784bed18
Jens Brandenburg <jens@vragments.com>
2024-09-12 17:00:38 +0200

    ToolsUsed edited stacking state

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,7 +1,7 @@
 import { useZustand } from '../lib/zustand';
 import classNames from '../lib/classNames';
 import { Post } from '../types/types';
-import { FC, useCallback, useState } from 'react';
+import { CSSProperties, FC, useCallback, useState } from 'react';
 import { MenuOpenedPost } from './Nav';
 import { Remark } from 'react-remark';
 import Lightbox from 'yet-another-react-lightbox';
@@ -10,22 +10,19 @@ import 'yet-another-react-lightbox/styles.css';
 import 'yet-another-react-lightbox/plugins/captions.css';
 
 const Content = () => {
-    const activePost = useZustand((state) => state.nav.activePost);
-
     return (
         <>
-            <main className={classNames('relative z-50 -mb-[2px] flex justify-center')}>{activePost && <ContentWrapper_Test post={activePost} />}</main>
+            <main className={classNames('relative z-50 -mb-[2px] flex justify-center')}>
+                <ContentWrapper_Test />
+            </main>
         </>
     );
 };
 
 export default Content;
 
-const ContentWrapper_Test: FC<{
-    post: Post;
-}> = ({ post }) => {
-    const { title, subTitle, toolsUsed, images, textBlocks, codeLink } = post;
-
+const ContentWrapper_Test: FC<{}> = () => {
+    const activePost = useZustand((state) => state.nav.activePost);
     const [topVal, setTopVal] = useState<number>(0);
     const [lightboxTo, setLightboxTo] = useState<number | null>(null);
 
@@ -35,12 +32,18 @@ const ContentWrapper_Test: FC<{
         }
     }, []);
 
+    if (!activePost) {
+        return null;
+    }
+
+    const { title, subTitle, toolsUsed, images, textBlocks, codeLink, date } = activePost;
+
     return (
         <div
             ref={contentRefCb}
             className={classNames(
                 'transform-[width,opacity] absolute bg-black/50 drop-shadow-lg duration-500',
-                post ? 'z-10 h-fit w-screen opacity-100' : '-z-10 h-0 w-full opacity-10',
+                activePost ? 'z-10 h-fit w-screen opacity-100' : '-z-10 h-0 w-full opacity-10',
             )}
         >
             <MenuOpenedPost hasImages={images ? true : false} codeLink={codeLink} setLightboxTo={setLightboxTo} />
@@ -56,13 +59,14 @@ const ContentWrapper_Test: FC<{
                 </div>
 
                 <div
-                    className='relative flex flex-wrap gap-y-6 overflow-y-auto bg-palette-utility-bg px-14 py-6 scrollbar-thin'
+                    className='relative flex flex-col gap-y-6 overflow-y-auto bg-palette-utility-bg px-14 py-6 scrollbar-thin'
                     style={{ height: window.innerHeight - topVal - 2 }}
                     // onBlur={() => store_activePost(null)} // TODO
                 >
-                    <div className='relative mt-2 w-full'>
+                    <div className='relative mt-2 flex w-full items-start justify-center'>
                         <ToolsUsed tools={toolsUsed} />
                         <h4 className='mx-auto text-center italic'>{subTitle}</h4>
+                        <div>{Date.parse(date)}</div>
                     </div>
 
                     {/* Text/Image Blocks */}
@@ -120,25 +124,28 @@ const ContentWrapper_Test: FC<{
 
 const ToolsUsed: FC<{ tools: Post['toolsUsed'] }> = ({ tools }) => {
     return tools ? (
-        <div className='group absolute left-0 top-1/2 my-auto flex -translate-y-1/2 select-none'>
-            <h6 className='mr-0.5 border border-transparent bg-palette-neutral-700/50 px-1 py-px text-neutral-100/75'>Built using:</h6>
-            <div className='relative grid grid-flow-col grid-cols-[repeat(4,1.5rem)] transition-[grid-template-columns,column-gap] duration-500 group-hover:grid-cols-[repeat(4,1fr)] group-hover:gap-x-0.5'>
-                {tools?.map((tool, idx) => {
-                    return (
-                        <h6
-                            key={tool + idx}
-                            className='overflow-hidden rounded-sm border border-black/50 bg-palette-neutral-700 px-1 py-px text-center text-neutral-400/50'
-                            style={
-                                {
-                                    // marginLeft: `calc(${1 * idx}rem * var(--enable))`,
-                                    // gridColumnStart: idx + 1,
-                                }
-                            }
-                        >
-                            {tool}
-                        </h6>
-                    );
-                })}
+        <div className='group select-none'>
+            <div className='pb-px text-xs uppercase italic text-neutral-100/75'>Built with</div>
+            <div className='relative'>
+                <div
+                    /* 
+						Using length -1 in --tools-count in order to have last element at full size, for 'stacked' look,
+						and margin-right -10000px to break out of parent element's width					
+					*/
+                    className='absolute mr-[-10000px] grid grid-flow-col grid-cols-[repeat(var(--tools-count),0.25fr)] gap-x-px overflow-x-visible text-neutral-400/50 transition-[grid-template-columns,column-gap] duration-500 group-hover:grid-cols-[repeat(var(--tools-count),1fr)] group-hover:gap-x-0.5 group-hover:text-neutral-400'
+                    style={{ '--tools-count': tools.length - 1 } as CSSProperties}
+                >
+                    {tools?.map((tool, idx) => {
+                        return (
+                            <div
+                                key={tool + idx}
+                                className='overflow-hidden whitespace-nowrap rounded-sm bg-palette-neutral-700 px-1 text-center text-xs leading-tight'
+                            >
+                                {tool}
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </div>
     ) : null;
