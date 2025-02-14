8bde9329b0eecf6900716529374b26560b367207
Jens Brandenburg <jens@vragments.com>
2025-02-14 17:28:46 +0100

    removed clause

--- src/entry.tsx
+++ src/entry.tsx
@@ -3,15 +3,8 @@ import App from './views/App.tsx';
 import './styles/style_main.css';
 import { StrictMode } from 'react';
 
-// WARN Get rid of before deploying
-const useStrict = true;
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    useStrict ? (
-        <StrictMode>
-            <App />
-        </StrictMode>
-    ) : (
+    <StrictMode>
         <App />
-    ),
+    </StrictMode>,
 );
