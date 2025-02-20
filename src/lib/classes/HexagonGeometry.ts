ae45043ab836719464fa6659bfb1de7dd054905e
Jens Brandenburg <jens@vragments.com>
2025-02-20 17:44:37 +0100

    HexagonGeometry class finished, takes padding into account now as well

--- src/lib/classes/HexagonGeometry.ts
+++ src/lib/classes/HexagonGeometry.ts
@@ -3,7 +3,6 @@ import { CircleGeometry } from 'three';
 // With guidance from https://eperezcosano.github.io/hex-grid/
 
 export default class HexagonGeometry extends CircleGeometry {
-    angle = (2 * Math.PI) / 6; // 60 deg
     radius;
     horizontalColumnOffset;
     verticalColumnOffset;
@@ -13,7 +12,7 @@ export default class HexagonGeometry extends CircleGeometry {
         super(radius, 6, thetaStart, thetaLength);
         this.radius = radius;
 
-        const angle = this.angle;
+        const angle = (2 * Math.PI) / 6; // 60 deg
         const rSin = radius * Math.sin(angle);
         const rCos = radius * Math.cos(angle);
 
@@ -26,11 +25,19 @@ export default class HexagonGeometry extends CircleGeometry {
         const horizontalColumnOffset = this.horizontalColumnOffset;
         const verticalColumnOffset = this.verticalColumnOffset;
         const rowOffset = this.rowOffset;
+        const radius = this.radius;
 
-        let evenOrUneven = column % 2 === 0 ? 0 : 1;
+        // Keep all hex onscreen when padding added
+        const hasPadding = padding > 0;
+        const evenOrUneven = column % 2 === 0 ? 0 : hasPadding ? 1 : -1;
 
-        const x = column * horizontalColumnOffset;
-        const y = verticalColumnOffset * evenOrUneven + row * rowOffset;
+        let x = column * (horizontalColumnOffset + padding);
+        let y = (verticalColumnOffset + padding / 2) * evenOrUneven + row * (rowOffset + padding);
+
+        if (!hasPadding) {
+            x -= radius;
+            y -= radius - verticalColumnOffset;
+        }
 
         return [x, y];
     }
