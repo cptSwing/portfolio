807e13b96db13a4a4921794c4b47d0dd8fbe601b
Jens Brandenburg <jens@vragments.com>
2025-02-23 17:08:58 +0100

    R3F compatible with react18

--- src/components/ThreeCanvas.tsx
+++ src/components/ThreeCanvas.tsx
@@ -1,86 +1,22 @@
-import { MouseEvent,  useEffect, useMemo, useRef, useState } from 'react';
-import {
-    BufferGeometry,
-    Color,
-    Intersection,
-    PointLight,
-    Scene,
-    ShaderMaterial,
-    Vector2,
-    WebGLRenderer,
-} from 'three';
+import { FC, MouseEvent, useMemo, useRef } from 'react';
+import { Color, Intersection, PointLight, Scene, Vector2, WebGLRenderer, PerspectiveCamera, Camera, Raycaster } from 'three';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import getInstancedMesh2, { getAdjacentIndices, getInstanceCount } from '../lib/getInstancedMesh2';
+import BackgroundMesh, { getAdjacentIndices, getInstanceCount } from '../lib/instancedMesh2';
 import { InstancedMesh2 } from '@three.ez/instanced-mesh';
-import { GridData } from '../types/types';
+import { GridData, InstancedMesh2ShaderMaterial, OriginalInstancePositions } from '../types/types';
 import { meshAnimations, setAnimationPattern } from '../lib/animateMeshes';
-import { PerspectiveCamera } from '@react-three/drei';
-
-const mousePosition = new Vector2(0, 0);
-const isSquare = false;
-
-let time_Ms_Global = 0;
-let hasRunOnce = false;
-let forwardAnimationRunning = 0;
-let gridData: GridData = {
-    overallWidth: 0,
-    overallHeight: 0,
-    instanceLength: 0.05,
-    instancePadding: 0.001,
-    gridCount: 300,
-    gridCountHorizontal: 0,
-    gridCountVertical: 0,
-    gridBaseColor: new Color(0x888888),
-};
-
+import { PerspectiveCamera as PerspectiveCameraImpl } from '@react-three/drei';
+import { useEvent } from 'react-use';
 
 const ThreeCanvas = () => {
-    const [wh, setWh] = useState([0, 0]);
-    const { camera } = useThree();
-
-    const mousePosition_Ref = useRef( new Vector2() )
-    const pointLight_Ref = useRef<PointLight>()
-    const raycaster_Ref = useRef()
-
-    const gridData_Memo = useMemo<GridData>(() => (), [wh])
-
-    useEffect(() => {
-        setWh(getWidthHeight(0, camera));
-    }, [ camera ] );
-    
-    useFrame( ( state, delta, frame ) => {
-        
-    })
-
     return (
-        <div className='fixed left-0 right-0 bottom-0 top-0 -z-50'>
-            <Canvas gl={ { alpha: true, antialias: true } } onMouseMove={  (ev: Event | MouseEvent) => {
-        const mouseEvent = ev as MouseEvent;
-        mouseEvent.preventDefault();
+        <div className='fixed bottom-0 left-0 right-0 top-0 -z-50'>
+            <Canvas gl={{ alpha: true, antialias: true }}>
+                <PerspectiveCameraImpl makeDefault position={[0, 0, 3]} fov={60} aspect={window.innerWidth / window.innerHeight} near={0.01} far={50} />
 
-        // Sets to [-1, 1] values, 0 at center
-        mousePosition_Ref.current.setX((mouseEvent.clientX / window.innerWidth) * 2 - 1);
-        mousePosition_Ref.current.setY(-(mouseEvent.clientY / window.innerHeight) * 2 + 1);
+                <Background isSquare={false} />
 
-        raycaster_Ref.current.setFromCamera(mousePosition_Ref.current, camera);
-        const intersection = raycaster_Ref.intersectObject(object, false);
-
-        handleIntersects(intersection, object);
-
-        // Converted to x,y at Scene Z:0
-        const lightPositionX = mousePosition.x * (gridData.overallWidth / 2);
-        const lightPositionY = mousePosition.y * (gridData.overallHeight / 2);
-
-        pointLight_Ref.current.position.setX(lightPositionX);
-        pointLight_Ref.current.position.setY(lightPositionY);
-    }}>
-                <PerspectiveCamera makeDefault position={ [ 0, 0, 3 ] } fov={ 60 } aspect={ window.innerWidth / window.innerHeight } near={ 0.01 } far={ 50 } />
-
-                <raycaster ref={raycaster_Ref} />
-
-                <pointLight ref={pointLight_Ref} position={[0, 0, 3]} color='white' intensity={0.5}  />
-                <ambientLight color='white' intensity={ 1 } />
-                
+                <ambientLight color='white' intensity={1} />
             </Canvas>
         </div>
     );
@@ -88,42 +24,100 @@ const ThreeCanvas = () => {
 
 export default ThreeCanvas;
 
-
-
-const mountThree = (div: HTMLDivElement) => {
-    // WARN not loaded when this is run
-    // const rootElement = document.documentElement;
-    // const bgGrey = rootElement.style.getPropertyValue('--theme-bg-base');
-    // color.setStyle(bgGrey);
-
-
-
-    gridData = getInstanceCount(gridData, isSquare);
-
-    const object = getInstancedMesh2(renderer, gridData, isSquare);
-
-    renderer.setAnimationLoop((time, frame) => threeAnimate(renderer, time, frame, scene, camera, object));
-
-
+const gridDataDefaults: GridData = {
+    overallWidth: 0,
+    overallHeight: 0,
+    instanceLength: 0.05,
+    instancePadding: 0,
+    gridCount: 300,
+    gridCountHorizontal: 0,
+    gridCountVertical: 0,
+    gridBaseColor: new Color(0x888888),
 };
 
+let time_Ms_Global = 0;
 
+const Background: FC<{ isSquare: boolean }> = ({ isSquare }) => {
+    const { camera } = useThree();
+    const mesh_OrigPositions_Ref = useRef<[InstancedMesh2ShaderMaterial | null, OriginalInstancePositions]>([null, []]);
+    const mousePosition_Ref = useRef(new Vector2());
+    const raycaster_Ref = useRef<Raycaster | null>(null);
+    const pointLight_Ref = useRef<PointLight | null>(null);
 
+    const gridData_Memo = useMemo<GridData>(() => {
+        const [width, height] = getWidthHeight(0, camera as PerspectiveCamera);
+        let gridData = { ...gridDataDefaults, overallWidth: width, overallHeight: height };
 
+        return getInstanceCount(gridData, isSquare);
+    }, [camera, isSquare]);
+
+    useEvent(
+        'mousemove',
+        (ev: Event | MouseEvent) => {
+            const mouseEvent = ev as MouseEvent;
+            mouseEvent.preventDefault();
+
+            // Sets to [-1, 1] values, 0 at center
+            mousePosition_Ref.current.setX((mouseEvent.clientX / window.innerWidth) * 2 - 1);
+            mousePosition_Ref.current.setY(-(mouseEvent.clientY / window.innerHeight) * 2 + 1);
+
+            if (mesh_OrigPositions_Ref.current && raycaster_Ref.current) {
+                const [mesh_Ref] = mesh_OrigPositions_Ref.current;
+                if (mesh_Ref) {
+                    raycaster_Ref.current.setFromCamera(mousePosition_Ref.current, camera);
+                    const intersection = raycaster_Ref.current.intersectObject(mesh_Ref, false);
+
+                    handleIntersects(intersection, mesh_Ref, gridData_Memo.gridCountHorizontal);
+                }
+            }
+
+            if (pointLight_Ref.current) {
+                // Converted to x,y at Scene Z:0
+                const lightPositionX = mousePosition_Ref.current.x * (gridData_Memo.overallWidth / 2);
+                const lightPositionY = mousePosition_Ref.current.y * (gridData_Memo.overallHeight / 2);
+
+                pointLight_Ref.current.position.setX(lightPositionX);
+                pointLight_Ref.current.position.setY(lightPositionY);
+            }
+        },
+        document,
+    );
+
+    useFrame(({ scene, gl, camera, clock }, _delta, frame) => {
+        if (mesh_OrigPositions_Ref.current) {
+            const [mesh_Ref, originalPositions_Ref] = mesh_OrigPositions_Ref.current;
+            mesh_Ref &&
+                originalPositions_Ref.length &&
+                threeAnimate(gl, clock.getElapsedTime(), frame, scene, camera, mesh_Ref, gridData_Memo, originalPositions_Ref);
+        }
+    });
+
+    return (
+        <>
+            <raycaster ref={raycaster_Ref} />;
+            <pointLight ref={pointLight_Ref} position={[0, 0, 3]} color='white' intensity={0.5} />
+            <BackgroundMesh ref={mesh_OrigPositions_Ref} gridData={gridData_Memo} isSquare={isSquare} />
+        </>
+    );
+};
+
+let hasRunOnce = false;
+let forwardAnimationRunning = 0;
 const threeAnimate = (
     renderer: WebGLRenderer,
     time_Ms: number,
-    _frame: XRFrame,
+    _frame: XRFrame | undefined,
     scene: Scene,
-    camera: PerspectiveCamera,
-    mesh: InstancedMesh2<{}, BufferGeometry, ShaderMaterial>,
+    camera: Camera,
+    mesh: InstancedMesh2ShaderMaterial,
+    gridData: GridData,
+    originalPositions_Ref: OriginalInstancePositions,
 ) => {
     mesh.material.uniforms.u_Time_Ms.value = time_Ms;
     time_Ms_Global = time_Ms;
 
     mesh.updateInstances((instance, idx) => {
         if (!hasRunOnce) {
-            // instance.position.setY(instance.position.y + gridData.overallHeight);
             instance.position.y += gridData.overallHeight;
 
             forwardAnimationRunning = 1;
@@ -138,6 +132,7 @@ const threeAnimate = (
             index: idx,
             time_Ms,
             gridData,
+            originalPosition: originalPositions_Ref[idx],
             ...(forwardAnimationRunning ? meshAnimations.tumble : meshAnimations.none),
         });
 
@@ -148,16 +143,15 @@ const threeAnimate = (
 };
 
 let intersected = 0;
-
 const tempHitVector = new Vector2(); // Stores current time in x., hit strength in y.
 const tempInstanceColor = new Color();
 
-const handleIntersects = (intersection: Intersection[], object: InstancedMesh2) => {
+const handleIntersects = (intersection: Intersection[], object: InstancedMesh2, gridCountHorizontal: number) => {
     if (intersection.length > 0) {
         const newInstanceId = intersection[0].instanceId ?? intersected;
 
         if (intersected !== newInstanceId) {
-            const adjacentIndices = getAdjacentIndices(newInstanceId, gridData.gridCountHorizontal, 2);
+            const adjacentIndices = getAdjacentIndices(newInstanceId, gridCountHorizontal, 2);
 
             object.getUniformAt(newInstanceId, 'u_Hit_Color', tempInstanceColor);
             object.setUniformAt(newInstanceId, 'u_Hit_Color', tempInstanceColor.setHex(0xffffff));
