06c465ff74c0d0a36c72a22df230e3a240f4aad5
Jens Brandenburg <jens@vragments.com>
2025-03-14 12:49:22 +0100

    new hook useForwardedRef()

---
+++ src/hooks/useForwardedRef.ts
@@ -0,0 +1,18 @@
+import { ForwardedRef, MutableRefObject, useEffect, useRef } from 'react';
+
+const useForwardedRef = <T>(ref: ForwardedRef<T>): MutableRefObject<T | null> => {
+    const innerRef = useRef<T | null>(null);
+
+    useEffect(() => {
+        if (!ref) return;
+        if (typeof ref === 'function') {
+            ref(innerRef.current);
+        } else {
+            ref.current = innerRef.current;
+        }
+    });
+
+    return innerRef;
+};
+
+export default useForwardedRef;
