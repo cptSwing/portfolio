a99bbf6d527558213daa5272e734ed5635c240e4
Jens Brandenburg <jens@vragments.com>
2025-07-08 15:03:34 +0200

    renamed

--- src/config/hexagonData.tsx
+++ src/config/hexagonData.tsx
@@ -1,6 +1,8 @@
-import configJSON from '../config/config.json';
-import { HexagonData, HexagonLink } from '../types/types';
-import { NavigationExpansionState } from '../views/Main';
+import configJSON from './config.json';
+import { HexagonData, HexagonLink, NavigationExpansionState } from '../types/types';
+import { useZustand } from '../lib/zustand';
+
+const { store_toggleMenu, store_setPostNavState } = useZustand.getState().methods;
 
 const {
     hexMenu: { columns, scaleUp },
@@ -50,9 +52,31 @@ const hexShape: ((Record<NavigationExpansionState, HexagonData> & HexagonLink) |
         {
             home: { position: allOffsets[0][1], rotation: 0, isHalf: true, scale: 1 },
             category: { position: allOffsets[1][0], rotation: 120, isHalf: true, scale: 0.9, offsets: { x: 2.325, y: -5.65 } },
-            post: { position: allOffsets[0][0], rotation: -30, isHalf: false, scale: 0.5, offsets: { x: -12.5, y: 4 } },
+            post: { position: allOffsets[0][0], rotation: -30, isHalf: false, scale: 0.35, offsets: { x: -12.5, y: 4 } },
         },
-        null,
+
+        // DisplayPost controls, only available in that component
+        {
+            home: { position: allOffsets[0][2], rotation: 0, isHalf: false, scale: 0 },
+            category: { position: allOffsets[0][2], rotation: 0, isHalf: false, scale: 0 },
+            post: { position: allOffsets[0][2], rotation: 0, isHalf: false, scale: 0.175, offsets: { x: 2.2, y: 3.375 } },
+            title: '<',
+            target: () => store_setPostNavState('prev'),
+        },
+        {
+            home: { position: allOffsets[0][2], rotation: 0, isHalf: false, scale: 0 },
+            category: { position: allOffsets[0][2], rotation: 0, isHalf: false, scale: 0 },
+            post: { position: allOffsets[0][2], rotation: 0, isHalf: false, scale: 0.2, offsets: { x: 5.9, y: 1 } },
+            title: 'x',
+            target: () => store_setPostNavState('close'),
+        },
+        {
+            home: { position: allOffsets[0][2], rotation: 0, isHalf: false, scale: 0 },
+            category: { position: allOffsets[0][2], rotation: 0, isHalf: false, scale: 0 },
+            post: { position: allOffsets[0][2], rotation: 0, isHalf: false, scale: 0.175, offsets: { x: 9.6, y: 3.375 } },
+            title: '>',
+            target: () => store_setPostNavState('next'),
+        },
     ],
 
     // 1
@@ -81,7 +105,7 @@ const hexShape: ((Record<NavigationExpansionState, HexagonData> & HexagonLink) |
         {
             home: { position: allOffsets[2][1], rotation: 0, isHalf: false, scale: 1 },
             category: { position: allOffsets[2][0], rotation: 120, isHalf: false, scale: 0.5, offsets: { x: -28.35, y: -6.4 } },
-            post: { position: allOffsets[0][2], rotation: -90, isHalf: false, scale: 0.5, offsets: { x: 12.5, y: 4 } },
+            post: { position: allOffsets[0][2], rotation: -90, isHalf: false, scale: 0.35, offsets: { x: 12.5, y: 4 } },
         },
         {
             home: { position: allOffsets[2][2], rotation: 60, isHalf: true, scale: 1 },
@@ -98,14 +122,14 @@ const hexShape: ((Record<NavigationExpansionState, HexagonData> & HexagonLink) |
             category: { position: allOffsets[7][0], rotation: 0, isHalf: false, scale: 0.8, offsets: { x: 1.35, y: -6.5 } }, // "Active" position
             post: { position: allOffsets[5][0], rotation: 0, isHalf: false, scale: 0.45, offsets: { x: 6.25, y: 1 } },
             title: 'code',
-            url: '0',
+            target: '0',
         },
         {
             home: { position: allOffsets[3][2], rotation: 60, isHalf: false, scale: 1 },
             category: { position: allOffsets[8][0], rotation: 0, isHalf: false, scale: 0.5, offsets: { x: -15.575, y: -2.5 } },
             post: { position: allOffsets[6][0], rotation: 0, isHalf: false, scale: 0.45, offsets: { x: -12.5, y: 0 } },
             title: '3d',
-            url: '1',
+            target: '1',
         },
         null,
     ],
@@ -127,16 +151,16 @@ const hexShape: ((Record<NavigationExpansionState, HexagonData> & HexagonLink) |
         {
             home: { position: allOffsets[4][1], rotation: -90, isHalf: false, scale: 0.25, offsets: { x: -3.125, y: 0 } },
             category: { position: allOffsets[1][3], rotation: 0, isHalf: false, scale: 0.35, offsets: { x: -6, y: 1.9 } },
-            post: { position: allOffsets[1][3], rotation: 0, isHalf: false, scale: 0.35, offsets: { x: -6.25, y: 0 } },
-            title: 'mnu',
-            url: 'menu',
+            post: { position: allOffsets[6][2], rotation: 0, isHalf: false, scale: 0.35, offsets: { x: 12.5, y: 0 } },
+            title: 'set',
+            target: () => store_toggleMenu('settings'),
         },
         {
             home: { position: allOffsets[4][1], rotation: 90, isHalf: false, scale: 0.25, offsets: { x: 3.125, y: 0 } },
             category: { position: allOffsets[1][3], rotation: 0, isHalf: false, scale: 0.3, offsets: { x: -12.5, y: 6.125 } },
-            post: { position: allOffsets[2][2], rotation: 0, isHalf: false, scale: 0.35, offsets: { x: 12.5, y: -3.5 } },
+            post: { position: allOffsets[7][3], rotation: 0, isHalf: false, scale: 0.35, offsets: { x: -6.25, y: -3.5 } },
             title: 'soc',
-            url: 'soc',
+            target: () => store_toggleMenu('socials'),
         },
 
         {
@@ -174,7 +198,7 @@ const hexShape: ((Record<NavigationExpansionState, HexagonData> & HexagonLink) |
             category: { position: allOffsets[7][0], rotation: 0, isHalf: false, scale: 0.37, offsets: { x: 11.3, y: 3.575 } },
             post: { position: allOffsets[7][0], rotation: 0, isHalf: false, scale: 0.45, offsets: { x: 6.25, y: -1 } },
             title: 'log',
-            url: '3',
+            target: '3',
         },
         {
             home: { position: allOffsets[6][2], rotation: 120, isHalf: true, scale: 1 },
@@ -189,12 +213,12 @@ const hexShape: ((Record<NavigationExpansionState, HexagonData> & HexagonLink) |
         {
             home: { position: allOffsets[7][1], rotation: 0, isHalf: false, scale: 1 },
             category: { position: allOffsets[5][0], rotation: -60, isHalf: false, scale: 0.75, offsets: { x: 1.45, y: -3 } },
-            post: { position: allOffsets[7][0], rotation: 90, isHalf: false, scale: 0.5, offsets: { x: 6.25, y: 8.5 } },
+            post: { position: allOffsets[7][0], rotation: 90, isHalf: false, scale: 0.35, offsets: { x: 6.25, y: 8.5 } },
         },
         {
             home: { position: allOffsets[7][2], rotation: 0, isHalf: false, scale: 1 },
             category: { position: allOffsets[6][2], rotation: -60, isHalf: true, scale: 1, offsets: { x: 13.9, y: 1.5 } },
-            post: { position: allOffsets[7][3], rotation: 30, isHalf: false, scale: 0.5, offsets: { x: -6.25, y: 8.5 } },
+            post: { position: allOffsets[7][3], rotation: 30, isHalf: false, scale: 0.35, offsets: { x: -6.25, y: 8.5 } },
         },
         null,
     ],
