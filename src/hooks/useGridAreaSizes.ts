16ae512b944ef8697ff4e1084c71b653a2300b3f
Jens Brandenburg <jens@vragments.com>
2025-07-18 11:57:13 +0200

    unused

--- src/hooks/useGridAreaSizes.ts
+++
@@ -1,6 +0,0 @@
-/*
-Problem: Updating boundingClientRect() on grid areas after their animation is complete, in order to update the svg clipping mask is too late. visible shift of clipping. 
-Solution: On Mount of Category, each gridArea's boundingClientRect is pre-calculated and returned via hook, and passed to <SVGClip../> . also add a boolean to force a re-calculation (via resize event for instance)
-*/
-
-const useGridAreaSizes = () => {};
