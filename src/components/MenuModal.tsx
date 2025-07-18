d61cd75b365b227a8df50bcb48e3cd5352f8dec6
Jens Brandenburg <jens@vragments.com>
2025-07-18 16:08:45 +0200

    rename, and used <dialog> instead of a div

--- src/components/MenuModal.tsx
+++ src/components/MenuModal.tsx
@@ -1,43 +1,40 @@
-import { createPortal } from 'react-dom';
 import { useZustand } from '../lib/zustand';
 import Settings from './Settings';
 import Socials from './Socials';
-import { CSSProperties, useCallback, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import RoundedHexagonSVG from './RoundedHexagonSVG';
 
 const store_toggleMenu = useZustand.getState().methods.store_toggleMenu;
 
-const MenuToggle = () => {
+const MenuModal = () => {
     const name = useZustand((store) => store.values.activeMenuButton.name);
-    const [twBgOpacity, setTwBgOpacity] = useState(0);
+    const dialogRef = useRef<HTMLDialogElement | null>(null);
 
-    const refCallback = useCallback((elem: HTMLDivElement | null) => {
-        if (elem) {
-            setTwBgOpacity(0.85);
-        } else {
-            setTwBgOpacity(0);
+    useEffect(() => {
+        if (dialogRef.current) {
+            if (name) {
+                dialogRef.current.showModal();
+                dialogRef.current.style.setProperty('--tw-bg-opacity', '0.9');
+            } else {
+                dialogRef.current.close();
+                dialogRef.current.style.removeProperty('--tw-bg-opacity');
+            }
         }
-    }, []);
-
-    const handleClick = ({ target, currentTarget }: React.MouseEvent) => currentTarget === target && store_toggleMenu({ name: null });
+    }, [name]);
 
     return (
-        name &&
-        createPortal(
-            <div
-                ref={refCallback}
-                className='fixed bottom-0 left-0 right-0 top-0 z-50 bg-gray-950 transition-[background-color] duration-300'
-                style={{ '--tw-bg-opacity': twBgOpacity } as CSSProperties}
-                onClick={handleClick}
-            >
-                {name === 'settings' ? <Settings /> : <Socials />}
-            </div>,
-            document.getElementById('root')!,
-        )
+        // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
+        <dialog
+            ref={dialogRef}
+            className='size-full bg-gray-950 bg-opacity-0 transition-[background-color]' // backdrop-blur-md
+            onClick={({ target, currentTarget }) => target === currentTarget && currentTarget.open && store_toggleMenu({ name: null })}
+        >
+            {name === 'settings' ? <Settings /> : <Socials />}
+        </dialog>
     );
 };
 
-export default MenuToggle;
+export default MenuModal;
 
 /* Used in child components: */
 export const CloseSubMenu = () => {
@@ -48,7 +45,7 @@ export const CloseSubMenu = () => {
     const handleClick = () => store_toggleMenu({ name: null });
 
     return (
-        <div
+        <button
             className='group absolute aspect-hex-flat cursor-pointer peer-hover-active:[--x-mark-opacity:0]'
             style={
                 menuButtonPosAndSize && {
@@ -64,6 +61,6 @@ export const CloseSubMenu = () => {
 
             {/* XMark */}
             <div className='size-full bg-theme-primary opacity-[--x-mark-opacity] transition-[background-color,opacity] [mask-image:url(/svg/XMarkOutline.svg)] [mask-position:center] [mask-repeat:no-repeat] [mask-size:55%] group-hover-active:bg-theme-primary-lighter' />
-        </div>
+        </button>
     );
 };
