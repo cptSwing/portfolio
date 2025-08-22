6df307317809b4e12fbdae9cd88c611e5789cec7
Jens Brandenburg <jens@vragments.com>
2025-08-22 17:39:44 +0200

    new hook for the resize observer

---
+++ src/hooks/useResizeObserver.ts
@@ -0,0 +1,25 @@
+import { useLayoutEffect, useRef, useState } from 'react';
+
+const useResizeObserver = (elem?: Element | null) => {
+    const [rect, setRect] = useState<DOMRect | null>(null);
+    const observer_Ref = useRef<ResizeObserver>();
+
+    useLayoutEffect(() => {
+        if (elem) {
+            if (!observer_Ref.current) {
+                observer_Ref.current = new ResizeObserver((entries) => entries.forEach((entry) => entry.target === elem && setRect(entry.contentRect)));
+            }
+
+            const observer = observer_Ref.current;
+            observer.observe(elem);
+
+            return () => {
+                observer.unobserve(elem);
+            };
+        }
+    }, [elem]);
+
+    return rect;
+};
+
+export default useResizeObserver;
