61a059785ec992728c42017e70d7b63045b9018a
cptSwing <jens@jbrandenburg.de>
2025-11-10 20:50:54 +0100

    background less busy

--- src/lib/menuFunctions.ts
+++ src/lib/menuFunctions.ts
@@ -2,11 +2,11 @@ import { CATEGORY } from '../types/enums';
 import { Category, CategoryName, ZustandStore } from '../types/types';
 
 export function getMenuButtonPosition(ev: React.MouseEvent<HTMLButtonElement | HTMLDivElement, MouseEvent>): ZustandStore['values']['hamburgerMenuRect'] {
-    const { left, width, top, height } = ev.currentTarget.firstElementChild
+    const { width, height } = ev.currentTarget.firstElementChild
         ? ev.currentTarget.firstElementChild.getBoundingClientRect()
         : ev.currentTarget.getBoundingClientRect();
 
-    return { x: left, y: top, width, height };
+    return { width, height };
 }
 
 export function isActiveCategory(name: CategoryName, category: Category) {
