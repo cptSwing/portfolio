7d6062e739692164b5f1a53716e19505df5c91b2
cptSwing <jens@jbrandenburg.de>
2025-11-26 17:18:29 +0100

    moved json db to public so I can edit it on server (yeah i know, sync call at the beginning, so shoot me)

--- src/types/exportTyped.ts
+++ src/types/exportTyped.ts
@@ -1,6 +1,38 @@
 import { DataBase, Config } from '../types/types';
-import testDatabaseUntyped from '../queries/testDb.json';
 import configUntyped from '../config/config.json';
 
-export const database = testDatabaseUntyped as DataBase;
+function getDatabaseSynchronous(): DataBase {
+    const request = new XMLHttpRequest();
+    request.open('GET', '/queries/testDb.json', false);
+    request.send(null);
+
+    if (request.status === 200) {
+        return JSON.parse(request.responseText);
+    } else {
+        // eslint-disable-next-line no-console
+        console.error('Json not loaded!');
+        return {
+            'code': {
+                id: 0,
+                title: '',
+                posts: [],
+                categoryBlurb: '',
+            },
+            '3d': {
+                id: 0,
+                title: '',
+                posts: [],
+                categoryBlurb: '',
+            },
+            'log': {
+                id: 0,
+                title: '',
+                posts: [],
+                categoryBlurb: '',
+            },
+        };
+    }
+}
+
+export const database = getDatabaseSynchronous();
 export const config = configUntyped as Config;
