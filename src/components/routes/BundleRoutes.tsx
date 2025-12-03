7942cb369873eb06d9411d1a30673ba746f7e9f4
cptSwing <jens@jbrandenburg.de>
2025-12-03 16:59:51 +0100

    lowercasing things

--- src/components/routes/BundleRoutes.tsx
+++ src/components/routes/BundleRoutes.tsx
@@ -2,9 +2,7 @@ import { useParams } from 'react-router-dom';
 
 const BundleRoutes = () => {
     const { bundlePath } = useParams();
-
-    bundlePath && window.location.assign(`${window.location.protocol}//${window.location.host}/bundles/${bundlePath}/index.html`);
-
+    bundlePath && window.location.assign(`${window.location.protocol}//${window.location.host}/bundles/${bundlePath.toLowerCase()}/index.html`);
     return <h3> Redirecting to {bundlePath}.... </h3>;
 };
 
