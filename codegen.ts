99cdfdabc075403f6321bbc0a9c9931fd915624e
cptSwing <jens@jbrandenburg.de>
2026-04-14 02:29:23 +0200

    graphql-codegen added, package updates, env variable (safe to share publicly)

---
+++ codegen.ts
@@ -0,0 +1,23 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+import { loadEnv } from 'vite';
+
+const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), 'ASTRO');
+
+const config: CodegenConfig = {
+    overwrite: true,
+    schema: env.ASTRO_GRAPHQL_ENDPOINT,
+    documents: 'src/**/*.{astro,graphql}',
+    generates: {
+        'src/api/gql/': { preset: 'client' },
+    },
+    config: {
+        enumsAsConst: true,
+        useTypeImports: true,
+        pureMagicComment: true,
+        printFieldsOnNewLines: true,
+        extractAllFieldsToTypes: true,
+    },
+    importExtension: '.ts',
+};
+
+export default config;
