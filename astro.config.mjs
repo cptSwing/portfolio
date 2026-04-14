eafff44dab46dbecd1405726a18e69b8a3d3ba62
cptSwing <jens@jbrandenburg.de>
2026-04-14 18:05:40 +0200

    .vscode cleanup, added settings to astro config (fonts, prefetch, build format)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,27 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import preact from '@astrojs/preact';
 
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
-
+    site: 'https://www.jbrandenburg.de',
+    output: 'static',
+    build: { format: 'preserve' },
     integrations: [preact()],
+    vite: { plugins: [tailwindcss()] },
+    devToolbar: { enabled: false },
+    prefetch: {
+        prefetchAll: true,
+        defaultStrategy: 'tap',
+    },
+    fonts: [
+        {
+            provider: fontProviders.google(),
+            name: 'Roboto',
+            weights: [200, '400', 'bold'],
+            styles: ['normal', 'italic', 'oblique'],
+            display: 'swap',
+            cssVariable: '--font-roboto',
+            fallbacks: ['sans-serif'],
+        },
+    ],
 });
