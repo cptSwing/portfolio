edeba575ceee156acea01ba9e1f1777efa5f3dea
cptSwing <jens@jbrandenburg.de>
2026-04-18 00:16:42 +0200

    unprettified api ts files

--- src/api/graphqlGenerated/gql.ts
+++ src/api/graphqlGenerated/gql.ts
@@ -14,20 +14,18 @@ import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-
  * Learn more about it here: https://the-guild.dev/graphql/codegen/plugins/presets/preset-client#reducing-bundle-size
  */
 type Documents = {
-    'query GetAllPostsSlugs {\n  posts {\n    nodes {\n      slug\n    }\n  }\n}': typeof types.GetAllPostsSlugsDocument;
-    'query GetAllPostsSlugsCategories {\n  posts {\n    nodes {\n      slug\n      categories {\n        nodes {\n          slug\n        }\n      }\n    }\n  }\n}': typeof types.GetAllPostsSlugsCategoriesDocument;
-    'query GetAllPostsSlugsTitlesCategoriesFeaturedImages {\n  posts {\n    nodes {\n      slug\n      title\n      categories {\n        nodes {\n          slug\n        }\n      }\n      featuredImage {\n        node {\n          sourceUrl\n        }\n      }\n    }\n  }\n}': typeof types.GetAllPostsSlugsTitlesCategoriesFeaturedImagesDocument;
-    'query GetAllPostsTitlesSlugs {\n  posts {\n    nodes {\n      title\n      slug\n    }\n  }\n}': typeof types.GetAllPostsTitlesSlugsDocument;
-    'query GetPost($slug: ID!) {\n  post(id: $slug, idType: SLUG) {\n    title\n    slug\n    content\n  }\n}': typeof types.GetPostDocument;
+    "query GetAllPostsSlugs {\n  posts {\n    nodes {\n      slug\n    }\n  }\n}": typeof types.GetAllPostsSlugsDocument,
+    "query GetAllPostsSlugsCategories {\n  posts {\n    nodes {\n      slug\n      categories {\n        nodes {\n          slug\n        }\n      }\n    }\n  }\n}": typeof types.GetAllPostsSlugsCategoriesDocument,
+    "query GetAllPostsSlugsTitlesCategoriesFeaturedImages {\n  posts {\n    nodes {\n      slug\n      title\n      categories {\n        nodes {\n          slug\n        }\n      }\n      featuredImage {\n        node {\n          sourceUrl\n        }\n      }\n    }\n  }\n}": typeof types.GetAllPostsSlugsTitlesCategoriesFeaturedImagesDocument,
+    "query GetAllPostsTitlesSlugs {\n  posts {\n    nodes {\n      title\n      slug\n    }\n  }\n}": typeof types.GetAllPostsTitlesSlugsDocument,
+    "query GetPost($slug: ID!) {\n  post(id: $slug, idType: SLUG) {\n    title\n    slug\n    content\n  }\n}": typeof types.GetPostDocument,
 };
 const documents: Documents = {
-    'query GetAllPostsSlugs {\n  posts {\n    nodes {\n      slug\n    }\n  }\n}': types.GetAllPostsSlugsDocument,
-    'query GetAllPostsSlugsCategories {\n  posts {\n    nodes {\n      slug\n      categories {\n        nodes {\n          slug\n        }\n      }\n    }\n  }\n}':
-        types.GetAllPostsSlugsCategoriesDocument,
-    'query GetAllPostsSlugsTitlesCategoriesFeaturedImages {\n  posts {\n    nodes {\n      slug\n      title\n      categories {\n        nodes {\n          slug\n        }\n      }\n      featuredImage {\n        node {\n          sourceUrl\n        }\n      }\n    }\n  }\n}':
-        types.GetAllPostsSlugsTitlesCategoriesFeaturedImagesDocument,
-    'query GetAllPostsTitlesSlugs {\n  posts {\n    nodes {\n      title\n      slug\n    }\n  }\n}': types.GetAllPostsTitlesSlugsDocument,
-    'query GetPost($slug: ID!) {\n  post(id: $slug, idType: SLUG) {\n    title\n    slug\n    content\n  }\n}': types.GetPostDocument,
+    "query GetAllPostsSlugs {\n  posts {\n    nodes {\n      slug\n    }\n  }\n}": types.GetAllPostsSlugsDocument,
+    "query GetAllPostsSlugsCategories {\n  posts {\n    nodes {\n      slug\n      categories {\n        nodes {\n          slug\n        }\n      }\n    }\n  }\n}": types.GetAllPostsSlugsCategoriesDocument,
+    "query GetAllPostsSlugsTitlesCategoriesFeaturedImages {\n  posts {\n    nodes {\n      slug\n      title\n      categories {\n        nodes {\n          slug\n        }\n      }\n      featuredImage {\n        node {\n          sourceUrl\n        }\n      }\n    }\n  }\n}": types.GetAllPostsSlugsTitlesCategoriesFeaturedImagesDocument,
+    "query GetAllPostsTitlesSlugs {\n  posts {\n    nodes {\n      title\n      slug\n    }\n  }\n}": types.GetAllPostsTitlesSlugsDocument,
+    "query GetPost($slug: ID!) {\n  post(id: $slug, idType: SLUG) {\n    title\n    slug\n    content\n  }\n}": types.GetPostDocument,
 };
 
 /**
@@ -47,36 +45,26 @@ export function graphql(source: string): unknown;
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(
-    source: 'query GetAllPostsSlugs {\n  posts {\n    nodes {\n      slug\n    }\n  }\n}'
-): (typeof documents)['query GetAllPostsSlugs {\n  posts {\n    nodes {\n      slug\n    }\n  }\n}'];
+export function graphql(source: "query GetAllPostsSlugs {\n  posts {\n    nodes {\n      slug\n    }\n  }\n}"): (typeof documents)["query GetAllPostsSlugs {\n  posts {\n    nodes {\n      slug\n    }\n  }\n}"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(
-    source: 'query GetAllPostsSlugsCategories {\n  posts {\n    nodes {\n      slug\n      categories {\n        nodes {\n          slug\n        }\n      }\n    }\n  }\n}'
-): (typeof documents)['query GetAllPostsSlugsCategories {\n  posts {\n    nodes {\n      slug\n      categories {\n        nodes {\n          slug\n        }\n      }\n    }\n  }\n}'];
+export function graphql(source: "query GetAllPostsSlugsCategories {\n  posts {\n    nodes {\n      slug\n      categories {\n        nodes {\n          slug\n        }\n      }\n    }\n  }\n}"): (typeof documents)["query GetAllPostsSlugsCategories {\n  posts {\n    nodes {\n      slug\n      categories {\n        nodes {\n          slug\n        }\n      }\n    }\n  }\n}"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(
-    source: 'query GetAllPostsSlugsTitlesCategoriesFeaturedImages {\n  posts {\n    nodes {\n      slug\n      title\n      categories {\n        nodes {\n          slug\n        }\n      }\n      featuredImage {\n        node {\n          sourceUrl\n        }\n      }\n    }\n  }\n}'
-): (typeof documents)['query GetAllPostsSlugsTitlesCategoriesFeaturedImages {\n  posts {\n    nodes {\n      slug\n      title\n      categories {\n        nodes {\n          slug\n        }\n      }\n      featuredImage {\n        node {\n          sourceUrl\n        }\n      }\n    }\n  }\n}'];
+export function graphql(source: "query GetAllPostsSlugsTitlesCategoriesFeaturedImages {\n  posts {\n    nodes {\n      slug\n      title\n      categories {\n        nodes {\n          slug\n        }\n      }\n      featuredImage {\n        node {\n          sourceUrl\n        }\n      }\n    }\n  }\n}"): (typeof documents)["query GetAllPostsSlugsTitlesCategoriesFeaturedImages {\n  posts {\n    nodes {\n      slug\n      title\n      categories {\n        nodes {\n          slug\n        }\n      }\n      featuredImage {\n        node {\n          sourceUrl\n        }\n      }\n    }\n  }\n}"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(
-    source: 'query GetAllPostsTitlesSlugs {\n  posts {\n    nodes {\n      title\n      slug\n    }\n  }\n}'
-): (typeof documents)['query GetAllPostsTitlesSlugs {\n  posts {\n    nodes {\n      title\n      slug\n    }\n  }\n}'];
+export function graphql(source: "query GetAllPostsTitlesSlugs {\n  posts {\n    nodes {\n      title\n      slug\n    }\n  }\n}"): (typeof documents)["query GetAllPostsTitlesSlugs {\n  posts {\n    nodes {\n      title\n      slug\n    }\n  }\n}"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(
-    source: 'query GetPost($slug: ID!) {\n  post(id: $slug, idType: SLUG) {\n    title\n    slug\n    content\n  }\n}'
-): (typeof documents)['query GetPost($slug: ID!) {\n  post(id: $slug, idType: SLUG) {\n    title\n    slug\n    content\n  }\n}'];
+export function graphql(source: "query GetPost($slug: ID!) {\n  post(id: $slug, idType: SLUG) {\n    title\n    slug\n    content\n  }\n}"): (typeof documents)["query GetPost($slug: ID!) {\n  post(id: $slug, idType: SLUG) {\n    title\n    slug\n    content\n  }\n}"];
 
 export function graphql(source: string) {
-    return (documents as any)[source] ?? {};
+  return (documents as any)[source] ?? {};
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<infer TType, any> ? TType : never;
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
